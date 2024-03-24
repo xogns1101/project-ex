55a80b72f6c0072f0ded1a8d24865edfb6222dd7
th__nks <cocoseasky@naver.com>
2024-03-24 13:48:50 +0900

    기본 구축환경 만들어보기

---
+++ src/DynamicBeat.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class DynamicBeat extends JFrame {
+
+    public DynamicBeat() {
+        setTitle("DynamicBeat");
+        setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+}
